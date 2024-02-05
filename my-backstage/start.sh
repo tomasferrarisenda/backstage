8ddc75e18d375cde02c82514590def3a689074cd
Tomas Ferrari <tferrari.92@gmail.com>
2024-02-05 16:51:00 +0100

    hola

--- my-backstage/start.sh
+++ my-backstage/start.sh
@@ -9,7 +9,7 @@ yarn tsc
 yarn build:backend
 # yarn build:backend --config ../../app-config.production.yaml
 
-docker image build . -f packages/backend/Dockerfile --tag tferrari92/backstage:22
-docker push tferrari92/backstage:22
+docker image build . -f packages/backend/Dockerfile --tag tferrari92/backstage:23
+docker push tferrari92/backstage:23
 
 git cmp "new tag"
