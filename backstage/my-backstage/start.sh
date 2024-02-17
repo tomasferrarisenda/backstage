8245125ebe45b1e1c98636a41256b5ce5fd7d8d1
Tomas Ferrari <tferrari.92@gmail.com>
2024-02-17 22:50:11 +0100

    Update app-config.production.yaml and start.sh files

--- backstage/my-backstage/start.sh
+++ backstage/my-backstage/start.sh
@@ -7,10 +7,10 @@ yarn tsc
 # Build the backend, which bundles it all up into the packages/backend/dist folder.
 # The configuration files here should match the one you use inside the Dockerfile below.
 # yarn build
-yarn build:backend
-# yarn build:backend --config ../../app-config.production.yaml
+# yarn build:backend
+yarn build:backend --config ../../app-config.production.yaml
 
-docker image build . -f packages/backend/Dockerfile --tag tferrari92/backstage:30
-docker push tferrari92/backstage:30
+docker image build . -f packages/backend/Dockerfile --tag tferrari92/backstage:32
+docker push tferrari92/backstage:32
 
 # git cmp "new tag"
