946ba7f5697654ff9409c354c42ecd7e98a1e3de
Tomas Ferrari <tferrari.92@gmail.com>
2024-02-17 23:03:00 +0100

    Update app-config.production.yaml and start.sh

--- backstage/my-backstage/start.sh
+++ backstage/my-backstage/start.sh
@@ -10,7 +10,7 @@ yarn tsc
 # yarn build:backend
 yarn build:backend --config ../../app-config.production.yaml
 
-docker image build . -f packages/backend/Dockerfile --tag tferrari92/backstage:35
-docker push tferrari92/backstage:35
+docker image build . -f packages/backend/Dockerfile --tag tferrari92/backstage:36
+docker push tferrari92/backstage:36
 
 # git cmp "new tag"
