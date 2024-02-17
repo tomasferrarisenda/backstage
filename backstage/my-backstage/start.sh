25638db92fb02d1eaae0c5f33fe761f74c26e1c9
Tomas Ferrari <tferrari.92@gmail.com>
2024-02-17 22:55:59 +0100

    Update Kubernetes configuration and Docker image tag

--- backstage/my-backstage/start.sh
+++ backstage/my-backstage/start.sh
@@ -10,7 +10,7 @@ yarn tsc
 # yarn build:backend
 yarn build:backend --config ../../app-config.production.yaml
 
-docker image build . -f packages/backend/Dockerfile --tag tferrari92/backstage:33
-docker push tferrari92/backstage:33
+docker image build . -f packages/backend/Dockerfile --tag tferrari92/backstage:34
+docker push tferrari92/backstage:34
 
 # git cmp "new tag"
