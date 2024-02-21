2645ef7b592c097789f00315087abc3e9e6aa200
Tomas Ferrari <tferrari.92@gmail.com>
2024-02-21 20:56:00 +0100

    Delete unnecessary files and configurations

---
+++ backstage/deploy-in-minikube.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Prompt the user for their GitHub token
+echo "Enter your GitHub token:"
+read GITHUB_TOKEN
+
+# Start cluster
+minikube start
+
+# We create the secret for the Github token with this command. This way the token won't get pushed to Github.
+ubectl create ns backstage
+kubectl create secret generic github-token --namespace=backstage --from-literal=GITHUB_TOKEN="$GITHUB_TOKEN"
+
+# Install Backstage
+helm install backstage -n argocd helm/infra/backstage --values helm/infra/backstage/values-custom.yaml --dependency-update --create-namespace
+
+# Wait for the Backstage pod to be ready
+echo "Waiting for backstage pod to be ready..."
+until [[ $(kubectl -n backstage get pods -l "app.kubernetes.io/name=backstage" -o 'jsonpath={..status.conditions[?(@.type=="Ready")].status}') == "True" ]]; do
+  echo "Waiting for backstage pod to be ready..."
+  sleep 3
+done
+
+# Port forward the Backstage service
+kubectl port-forward -n backstage service/backstage 8080:7007
