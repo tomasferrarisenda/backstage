6e47aac0efdb12c1da40a720c49a10d5f2025119
Tomas Ferrari <tferrari.92@gmail.com>
2024-02-21 21:25:38 +0100

    Delete unnecessary secret, configmap, service, and deployment files

--- backstage/deploy-in-minikube.sh
+++ backstage/deploy-in-minikube.sh
@@ -9,10 +9,16 @@ minikube start
 
 # We create the secret for the Github token with this command. This way the token won't get pushed to Github.
 kubectl create ns backstage
-kubectl create secret generic github-token --namespace=backstage --from-literal=GITHUB_TOKEN="$GITHUB_TOKEN"
+kubectl create secret generic github-token -n backstage --from-literal=GITHUB_TOKEN="$GITHUB_TOKEN"
 
 # Install Backstage
-helm install backstage -n argocd helm/infra/backstage --values helm/infra/backstage/values-custom.yaml --dependency-update --create-namespace
+helm install backstage -n backstage helm/infra/backstage --values helm/infra/backstage/values-custom.yaml --dependency-update --create-namespace
+
+# Install Redis
+helm install redis -n my-app helm/my-app/redis --dependency-update --create-namespace
+
+# Install Redis
+helm install backend -n my-app helm/my-app/backend --dependency-update --create-namespace
 
 # Wait for the Backstage pod to be ready
 echo "Waiting for backstage pod to be ready..."
